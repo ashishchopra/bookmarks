9e09f3c7b5da711bbefefbb553cf59f270df95d0
Ashish Chopra <ashishchopra0308@gmail.com>
2025-09-11 17:53:58 +0530

    user profile creation and edit functionality

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
@@ -39,5 +41,12 @@ urlpatterns = [
     # ),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
